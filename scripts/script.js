912ecd446c055df04c1ea6a3b1af5705718fb7a8
sharedordaz <sharedordaz@live.com.mx>
2021-11-18 20:56:37 -0600

    finished responsive menu

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let menu_icon = document.querySelector(".menu");
+let menu = document.querySelectorAll("ul a");
+console.log(menu);
+menu_icon.addEventListener("click", show_menu)
+
+function show_menu() {
+    menu.forEach( item => {
+        if (item.className === ""){
+            item.className = "display-menu";
+    }
+    else{
+        item.className = "";
+    }
+
+    });
+}
